0499f242927c8e398e6d9905e83ff15e538ae871
Chawan Saeed <chawan.saeed02@gmail.com>
2021-02-03 21:18:51 +0300

    Initial Commit

---
+++ Food_Recipes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food_Recipes
+//
+//  Created by Chawan Saeed on 2/3/21.
+//  Copyright © 2021 Chawan Saeed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
